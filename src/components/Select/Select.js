5c5e4a7b27c3ad0a114a34ed5dd93f3b2b625959
Alexandre Lim <alexandre.lim94@gmail.com>
2021-05-30 19:45:44 +0200

    Add Select without focus

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -1,18 +1,56 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
-import { COLORS } from '../../constants';
-import Icon from '../Icon';
-import { getDisplayedValue } from './Select.helpers';
+import { COLORS } from "../../constants";
+import Icon from "../Icon";
+import { getDisplayedValue } from "./Select.helpers";
 
 const Select = ({ label, value, onChange, children }) => {
   const displayedValue = getDisplayedValue(value, children);
 
   return (
-    <select value={value} onChange={onChange}>
-      {children}
-    </select>
+    <Wrapper>
+      <SelectIcon id="chevron-down" />
+      <NativeSelect value={value} onChange={onChange}>
+        {children}
+      </NativeSelect>
+      <Presentation>{displayedValue}</Presentation>
+    </Wrapper>
   );
 };
+
+const Wrapper = styled.div`
+  position: relative;
+  color: ${COLORS.gray700};
+  width: fit-content;
+
+  &:hover {
+    color: ${COLORS.black};
+  }
+`;
+
+const NativeSelect = styled.select`
+  appearance: none;
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  opacity: 0;
+`;
+
+const Presentation = styled.div`
+  border-radius: 8px;
+  background-color: ${COLORS.transparentGray15};
+  padding: 12px 52px 12px 16px;
+  font-size: 1rem;
+  line-height: 19px;
+`;
+
+const SelectIcon = styled(Icon)`
+  position: absolute;
+  top: 10px;
+  right: 16px;
+`;
 
 export default Select;
